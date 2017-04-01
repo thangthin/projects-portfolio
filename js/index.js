84dae354999170250947b0ac05520a8567f0514d
thangthin <thangthin@gmail.com>
2017-04-01 13:44:00 -0500

    removed redundant divs, added class name to div and section according to rubric

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 (function() {
     // project details for modal display
     const PROJECT_DETAILS = {
-        "APPIFY": "This is APPIFY project description",
-        "SUNFLOWER": "This is SUNFLOWER's project description",
-        "BOKEH": "VR demonstration"
+        "APPIFY": "This is APPIFY project description Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "SUNFLOWER": "This is SUNFLOWER's project description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "BOKEH": "VR demonstration: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     };
 
     // getting the elements
     let modal = document.querySelector(".modal");
     let closeButton = document.querySelector(".close");
     let modalContentDetail = document.querySelector("#modal-content-detail");
-
+    let modalContentTitle = document.querySelector("#modal-content-title");
     //helpers
     function toggleModal() {
         // toggle element style display property between: none and block
@@ -19,7 +19,9 @@
 
     function setModalContent(evt) {
         // set the modal content to the app name
-        modalContentDetail.textContent = PROJECT_DETAILS[evt.target.textContent];
+        let projectName = evt.target.textContent
+        modalContentTitle.textContent = projectName
+        modalContentDetail.textContent = PROJECT_DETAILS[projectName];
     }
 
     // set modal to watch click
@@ -27,7 +29,7 @@
 
     // for each project, set the toggle handler for when link is clicked
     // set the modal content to the appropriate project detail
-    [...document.querySelectorAll("article>div>a")].forEach(
+    [...document.querySelectorAll(".project-title")].forEach(
         (a) => {
             // overwrite default behavior of empty anchor link click handler
             a.addEventListener("click", (evt) => {
